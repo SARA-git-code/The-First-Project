a43ecd95d8ffb18bca7b69d529c88980324c7df9
SARA-git-code <sara.salehd66@gmail.com>
2024-09-14 12:29:15 +0300

    second stage

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    EditText let_text;
+    Button press_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+      text=findViewById(R.id.textView);
+      let_text=findViewById(R.id.editTextText);
+      press_button=findViewById(R.id.button);
+ press_button.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View view) {
+         Toast.makeText(getApplicationContext(),let_text.getText(),Toast.LENGTH_LONG).show();
+     }
+ });
     }
 }
